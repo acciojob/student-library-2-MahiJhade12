a74d8a5f38c3fc00ed16f1262d783b7ccd39ec58
MAHIMA JhADE <mahimajhade123@gmail.com>
2023-01-27 18:34:05 +0530

    student

--- src/main/java/com/driver/controller/BookController.java
+++ src/main/java/com/driver/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.driver.controller;
 
 import com.driver.models.Book;
+import com.driver.models.Student;
+import com.driver.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +12,27 @@ import java.util.List;
 
 //Add required annotations
 
+@RestController
+@RequestMapping("/books")
 public class BookController {
 
+    @Autowired
+    BookService bookService;
 
     //Write createBook API with required annotations
+    @RequestMapping("/addBook")
+    public ResponseEntity<String> createBook(@RequestBody Book book){
+        bookService.createBook(book);
+        return new ResponseEntity<>("the book is successfully added to the system", HttpStatus.CREATED);
+    }
 
     //Add required annotations
+    @GetMapping("/getBook")
     public ResponseEntity getBooks(@RequestParam(value = "genre", required = false) String genre,
                                    @RequestParam(value = "available", required = false, defaultValue = "false") boolean available,
                                    @RequestParam(value = "author", required = false) String author){
 
-        List<Book> bookList = null; //find the elements of the list by yourself
+        List<Book> bookList = bookService.getBooks(genre,available,author); //find the elements of the list by yourself
 
         return new ResponseEntity<>(bookList, HttpStatus.OK);
 
